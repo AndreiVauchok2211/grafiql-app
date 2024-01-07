2b794db7a823b309aae34e47ad3374404967d3ae
AndreiVauchok2211 <vauchokandrei@gmail.com>
2024-01-07 23:11:21 +0300

    fix: fix path to image

--- src/components/Graphi/Graphi.tsx
+++ src/components/Graphi/Graphi.tsx
@@ -1,6 +1,7 @@
 import { GraphJson } from '../GraphJson';
 import { GraphDoca } from '../GraphDoca';
 import styles from './Graphi.module.scss';
+import startSvg from '../../../public/startSvg.svg';
 
 interface GraphiPageProps {
   translate: (key: string) => string;
@@ -12,7 +13,8 @@ export function Graphi({ translate }: GraphiPageProps) {
       <div className={styles.graphi_page}>
         <div className={styles.graphi__sidebar}>
           <button className={styles.graphi_start_btn}>
-            <img src="../../../public/start-svg.svg" alt="book" />
+            {/* <img src="../../../public/start-svg.svg" alt="book" /> */}
+            <img src={startSvg} alt="book" />
           </button>
           <button className={styles.btn__doc_open}>
             <img src="../../../public/book-open-svg.svg" alt="book" />
