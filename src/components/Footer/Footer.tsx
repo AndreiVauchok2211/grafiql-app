c51a78c242f1381da8cb9c06f82e4b1ab24bc65c
AndreiVauchok2211 <vauchokandrei@gmail.com>
2024-01-07 23:46:46 +0300

    fix: fix path to image and styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,7 @@
 import { clsx } from '../../utils/clsx';
 import styles from './Footer.module.scss';
+import rsSchoolJs from '../../../public/rsSchoolJs.svg';
+import github from '../../../public/github.svg';
 
 interface FooterPageProps {
   translate: (key: string) => string;
@@ -13,30 +15,18 @@ export function Footer({ translate }: FooterPageProps) {
           className={styles.footer__link}
           href="https://github.com/SergeyVolkov03"
         >
-          <img
-            className={styles.footer__img}
-            src="../../../public/github-142-svgrepo-com.svg"
-            alt="photo author"
-          />
+          <img className={styles.footer__img} src={github} alt="photo author" />
           Sergey Volkov
         </a>
         <a className={styles.footer__link} href="https://github.com/Fuzailkhon">
-          <img
-            className={styles.footer__img}
-            src="../../../public/github-142-svgrepo-com.svg"
-            alt="photo author"
-          />
+          <img className={styles.footer__img} src={github} alt="photo author" />
           Fuzailkhon
         </a>
         <a
           className={styles.footer__link}
           href="https://github.com/AndreiVauchok2211"
         >
-          <img
-            className={styles.footer__img}
-            src="../../../public/github-142-svgrepo-com.svg"
-            alt="photo author"
-          />
+          <img className={styles.footer__img} src={github} alt="photo author" />
           Andrei Vauchok
         </a>
       </div>
@@ -47,7 +37,7 @@ export function Footer({ translate }: FooterPageProps) {
         <a className={styles.footer__link} href="https://rs.school/react/">
           <img
             className={clsx(styles.footer_rs, styles.footer__img)}
-            src="../../../public/rs_school_js.svg"
+            src={rsSchoolJs}
             alt="photo author"
           />
           {translate('footer_course')}
--- src/components/Graphi/Graphi.tsx
+++ src/components/Graphi/Graphi.tsx
@@ -2,6 +2,8 @@ import { GraphJson } from '../GraphJson';
 import { GraphDoca } from '../GraphDoca';
 import styles from './Graphi.module.scss';
 import startSvg from '../../../public/startSvg.svg';
+import bookOpen from '../../../public/bookOpen.svg';
+import bookClose from '../../../public/bookClose.svg';
 
 interface GraphiPageProps {
   translate: (key: string) => string;
@@ -13,14 +15,13 @@ export function Graphi({ translate }: GraphiPageProps) {
       <div className={styles.graphi_page}>
         <div className={styles.graphi__sidebar}>
           <button className={styles.graphi_start_btn}>
-            {/* <img src="../../../public/start-svg.svg" alt="book" /> */}
-            <img src={startSvg} alt="book" />
+            <img src={startSvg} alt="start" />
           </button>
           <button className={styles.btn__doc_open}>
-            <img src="../../../public/book-open-svg.svg" alt="book" />
+            <img src={bookOpen} alt="book_open" />
           </button>
           <button className={styles.btn__doc_close}>
-            <img src="../../../public/book-svg.svg" alt="book" />
+            <img src={bookClose} alt="book_close" />
           </button>
         </div>
         <GraphDoca translate={translate} />
